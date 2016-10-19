9541bfd9c4e0fa18be95e1ae70acb02b30ae1960
Tim Cope <kirbycope@gmail.com>
2016-10-19 14:02:31 -0600

    Added Grid Support

--- workspace/AutomationFramework/src/JUnit/TestBase.java
+++ workspace/AutomationFramework/src/JUnit/TestBase.java
@@ -1,21 +1,31 @@
 package JUnit;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestBase {
 	
 	protected WebDriver driver;
 	protected String baseUrl = "http://timothycope.com/";
+	protected String hubUrl = "http://localhost:4444/wd/hub";
 	
 	@Before
 	public void setUp() throws Exception{
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
+		try {
+			DesiredCapabilities capability = DesiredCapabilities.chrome();
+			driver = new RemoteWebDriver(new URL(hubUrl), capability);
+		}
+		catch (Exception e){
+			System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
